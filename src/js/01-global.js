afd549238f5e96b4ed17b705fe3e7115fe638a05
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-07-18 00:26:41 +0200

    Correct get data API

--- src/js/01-global.js
+++ src/js/01-global.js
@@ -2,7 +2,7 @@
 
 /* Elementos que usamos del HTML */
 
-const resultsList = document.querySelector('.js-results-list');
+
 // const inputAnime = document.querySelector('.js-input');
 // const btnSearch = document.querySelector('.js-btn-search');
 // const favoriteList = document.querySelector('.js-favorite-list');
--- src/js/02-search.js
+++ src/js/02-search.js
@@ -1,39 +1,44 @@
 'use strict';
 
 let animes = [];
+const resultsList = document.querySelector('.js-results-list');
 
-function renderAnime() {
-    let html = '';
-    for (const oneAnime of animes) {
-        html += ` <li class="js-one-anime">`;
-        html += ` <div class="js-container-anime">`;
-        html += ` <h3>${oneAnime.title}</h3>`;
-        html += ` <img class="anime_img" src=${animeImage} alt="Portada de la serie de anime ${oneAnime.title}"/>`;
-        html += `</div></li>`;        
-    }
-    resultsList.innerHTML = html;
-    // listenerAnimes();
+function renderAnime(animes) {
+  let html = '';
+  for (const oneAnime of animes) {
+    let imageUrl = animeImage(oneAnime);
+
+    html += ` <li class="js-one-anime">`;
+    html += ` <div class="js-container-anime">`;
+    html += ` <h3>${oneAnime.title}</h3>`;
+    html += ` <img class="anime_img" src="${imageUrl}" alt="Portada de la serie de anime ${oneAnime.title}" title="Portada de la serie de anime ${oneAnime.title}"/>`;
+    html += `</div></li>`;
+  }
+  resultsList.innerHTML = html;
+  // listenerAnimes();
 }
 
 function animeImage(data) {
-    if (
-      data.images.jpg.image_url ===
-      "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"
-    ) {
-      ("https://via.placeholder.com/210x295/ffffff/666666/?text=TV");
-    } else {
-      return data.images.jpg.image_url;
-    }
+  if (
+    data.images.jpg.image_url ===
+    'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png'
+  ) {
+    return 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+  } else {
+    return data.images.jpg.image_url;
   }
+}
 
 function getDataApi() {
-    fetch(
-      'https://api.jikan.moe/v4/anime?q=naruto'
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        let animes = data.data;
-      });
-  }
-  
-  getDataApi();
+  fetch(
+
+    'https://api.jikan.moe/v4/anime?'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let animes = data.data;
+      renderAnime(animes);
+    });
+}
+
+getDataApi();
